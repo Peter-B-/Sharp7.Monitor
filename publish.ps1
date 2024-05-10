9a1ee565fc044c0e4e8402204d314aa0170fbcd3
Peter Butzhammer <peter@butzhammer.de>
2024-05-10 22:05:09 +0200

    Update local build script

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,6 @@
 
 dotnet publish .\Sharp7.Monitor\Sharp7.Monitor.csproj -c Release --output publish\non-sc --no-self-contained
 
-dotnet publish .\Sharp7.Monitor\Sharp7.Monitor.csproj -c Release --output publish\sc -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true --self-contained
+dotnet publish .\Sharp7.Monitor\Sharp7.Monitor.csproj -c Release -r win-x64 --self-contained  -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true  --output publish\sc\win-x64
+dotnet publish .\Sharp7.Monitor\Sharp7.Monitor.csproj -c Release -r linux-x64 --self-contained  -p:PublishTrimmed=true -p:EnableCompressionInSingleFile=true  --output publish\sc\linux-x64
+
